d9fa1480f3a7ebddb44c957ca40a70453cd11ad3
Emil Ismayilzada <ismailzadeh.emil@gmail.com>
2023-04-04 15:36:36 +0200

    drag and drop, lines

--- Initial/main.cpp
+++ Initial/main.cpp
@@ -1,147 +1,152 @@
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <stdio.h>
-#include <math.h>
-#define PI 3.141592655359
+
+#define WIDTH 640
+#define HEIGHT 640
+
+GLuint vao, vbo;
+
+GLfloat points[12] = { -0.75f, 0.0f, 0.0f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f, 0.0f, -0.5f, 0.8f, 0.0f };
+
+GLint dragged = -1;
+
+GLfloat dist2_2d(GLfloat P1x, GLfloat P1y, GLfloat P2x, GLfloat P2y) {
+
+    GLfloat dx = P1x - P2x;
+    GLfloat dy = P1y - P2y;
+    return dx * dx + dy * dy;
+}
+
+GLint getActivePoint(GLfloat* p, GLfloat sensitivity, GLfloat x, GLfloat y) {
+
+    GLfloat		s = sensitivity * sensitivity;
+    GLfloat		xNorm = -1 + x / (WIDTH / 2);
+    GLfloat		yNorm = -1 + (HEIGHT - y) / (HEIGHT / 2);
+
+    for (GLint i = 0; i < 4; i++)
+        if (dist2_2d(p[i * 3], p[i * 3 + 1], xNorm, yNorm) < s)
+            return i;
+
+    return -1;
+}
+
+void cursorPosCallback(GLFWwindow* window, double x, double y) {
+
+    if (dragged >= 0) {
+
+        GLfloat		xNorm = -1 + x / (WIDTH / 2);
+        GLfloat		yNorm = -1 + (HEIGHT - y) / (HEIGHT / 2);
+
+        points[3 * dragged] = xNorm;  // x coord
+        points[3 * dragged + 1] = yNorm;  // y coord
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(GLfloat), points, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        double	x, y;
+
+        glfwGetCursorPos(window, &x, &y);
+        dragged = getActivePoint(points, 0.1f, x, y);
+    }
+
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+        dragged = -1;
+}
 
 int main() {
     GLFWwindow* window = NULL;
     const GLubyte* renderer;
     const GLubyte* version;
-    GLuint vao;
-    GLuint vbo;
-    /* geometry to use. these are 3 xyz points (9 floats total) to make a
-       triangle */
-    GLfloat points[] = { 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f };
 
-    GLfloat x0 = 0.0f; // Center's x coordinate
-    GLfloat y0 = 0.0f; // Center's y coordinate
-    GLfloat r = 0.6f; // radius
-    GLfloat circle[300];
-
-    GLfloat t;
-    GLint i;
-    GLfloat step = (2*PI)/100.0f;
-
-    /* these are the strings of code for the shaders
-    the vertex shader positions each vertex point */
     const char* vertex_shader =
         "#version 330\n"
-        "attribute vec3 vp;"
+        "in vec3 vp;"
         "void main () {"
-        "  gl_Position = vec4 (vp, 1.0);"
+        "  gl_Position = vec4(vp, 1.0);"
         "}";
-    /* the fragment shader colours each fragment (pixel-sized area of the
-    triangle) */
+
     const char* fragment_shader =
         "#version 330\n"
+        "out vec4 frag_colour;"
         "void main () {"
-        "  gl_FragColor = vec4 (0.0, 0.0, 1.0, 1.0);"
+        "  frag_colour = vec4(1.0, 1.0, 0.0, 1.0);"
         "}";
-    /* GL shader objects for vertex and fragment shader [components] */
-    GLuint vs, fs;
-    /* GL shader programme object [combined, to link] */
+
+    GLuint vert_shader, frag_shader;
     GLuint shader_programme;
 
-    /* start GL context and O/S window using the GLFW helper library */
     if (!glfwInit()) {
         fprintf(stderr, "ERROR: could not start GLFW3\n");
         return 1;
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(1000, 1000, "Hello Triangle", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Drag&Drop", NULL, NULL);
     if (!window) {
         fprintf(stderr, "ERROR: could not open window with GLFW3\n");
         glfwTerminate();
         return 1;
     }
     glfwMakeContextCurrent(window);
-    // start GLEW extension handler
+
     glewExperimental = GL_TRUE;
     glewInit();
 
-    /* get version info */
-    renderer = glGetString(GL_RENDERER); /* get renderer string */
-    version = glGetString(GL_VERSION);  /* version as a string */
-    printf("Renderer: %s\n", renderer);
-    printf("OpenGL version supported %s\n", version);
-
-    // tell GL to only draw onto a pixel if the shape is closer to the viewer
-    glEnable(GL_DEPTH_TEST); // enable depth-testing
-    // depth-testing interprets a smaller value as "closer"
+    glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
-
-    ///////////////////
-    ///////////////////
-    ///////////////////
-
-    for (i = 0; i < 100; i++)
-    {
-        t = i * step;
-        circle[i * 3] = x0 + r * cos(t);
-        circle[i * 3 + 1] = y0 + r * sin(t);
-        circle[i * 3 + 2] = 0.0f;
-
-    }
-
-    /* a vertex buffer object (VBO) is created here. this stores an array of
-       data on the graphics adapter's memory. in our case - the vertex points */
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 300 * sizeof(GLfloat), circle, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(GLfloat), points, GL_STATIC_DRAW);
 
-    /* the vertex array object (VAO) is a little descriptor that defines which
-    data from vertex buffer objects should be used as input variables to vertex
-    shaders. in our case - use our only VBO, and say 'every three floats is a
-    variable' */
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
+
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-    /* here we copy the shader strings into GL shaders, and compile them. we
-       then create an executable shader 'program' and attach both of the compiled
-       shaders. we link this, which matches the outputs of the vertex shader to
-       the inputs of the fragment shader, etc. and it is then ready to use */
-    vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertex_shader, NULL);
-    glCompileShader(vs);
-    fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragment_shader, NULL);
-    glCompileShader(fs);
+    vert_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vert_shader, 1, &vertex_shader, NULL);
+    glCompileShader(vert_shader);
+
+    frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag_shader, 1, &fragment_shader, NULL);
+    glCompileShader(frag_shader);
+
     shader_programme = glCreateProgram();
-    glAttachShader(shader_programme, fs);
-    glAttachShader(shader_programme, vs);
+    glAttachShader(shader_programme, frag_shader);
+    glAttachShader(shader_programme, vert_shader);
     glLinkProgram(shader_programme);
 
+    glfwSetCursorPosCallback(window, cursorPosCallback);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
 
-    /* this loop clears the drawing surface, then draws the geometry described
-       by the VAO onto the drawing surface. we 'poll events' to see if the window
-       was closed, etc. finally, we 'swap the buffers' which displays our drawing
-       surface onto the view area. we use a double-buffering system which means
-       that we have a 'currently displayed' surface, and 'currently being drawn'
-       surface. hence the 'swap' idea. in a single-buffering system we would see
-       stuff being drawn
-    one-after-the-other */
+    glPointSize(15.0f);
+
     while (!glfwWindowShouldClose(window)) {
-        // wipe the drawing surface clear
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         glUseProgram(shader_programme);
         glBindVertexArray(vao);
-        // draw points 0-3 from the currently bound VAO with current shader
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 100);
-        // update other events like input handling
+        glDrawArrays(GL_LINES, 0, 4);
+        glBindVertexArray(0);
+
         glfwPollEvents();
-        // put the stuff we've been drawing onto the display
         glfwSwapBuffers(window);
     }
 
-    // close GL context and any other GLFW resources
     glfwTerminate();
     return 0;
 }
